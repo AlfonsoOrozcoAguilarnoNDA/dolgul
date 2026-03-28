532e7210d2d116ebb28cc8a3fd867a65ff9e7800
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 08:27:36 -0600

    Update headergrok.php
    jsdelivr

--- headergrok.php
+++ headergrok.php
@@ -42,8 +42,8 @@ $es_master    = ($rol === 'Master');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Tickets</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="ticketsgrok.css">
 </head>
 <body>
