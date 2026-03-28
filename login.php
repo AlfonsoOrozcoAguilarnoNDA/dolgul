beae7c47b39ca0a95141b843e25666fb8eaf3224
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 08:33:08 -0600

    Update login.php
    jsdelivr y 3.7.1 en jquery

--- login.php
+++ login.php
@@ -130,8 +130,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </button>
     </form>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
