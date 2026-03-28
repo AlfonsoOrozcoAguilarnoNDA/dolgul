8c460e87c5af583a263e6e3a1d1c87a3a221b007
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 08:32:24 -0600

    Update footergrok.php
    jquery a 3.71 y jsdelivr

--- footergrok.php
+++ footergrok.php
@@ -33,8 +33,7 @@
         ?></span>
     </div>
 </footer>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
